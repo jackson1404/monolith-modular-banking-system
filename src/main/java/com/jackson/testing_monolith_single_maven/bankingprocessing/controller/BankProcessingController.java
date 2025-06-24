831ea1eca9a922beba625ede8188d26ade5f96bc
jackson1404 <jackson14042004@gmail.com>
2025-06-24 22:00:50 +0630

    added bank processing controller

---
+++ src/main/java/com/jackson/testing_monolith_single_maven/bankingprocessing/controller/BankProcessingController.java
@@ -0,0 +1,11 @@
+package com.jackson.testing_monolith_single_maven.bankingprocessing.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class BankProcessingController {
+
+    
+
+
+}
