b191dd5633b97daa89a316befaa4c511460966a2
jackson1404 <jackson14042004@gmail.com>
2025-06-24 22:57:24 +0630

    added location contoroller for near by and servcie for frame

--- src/main/java/com/jackson/testing_monolith_single_maven/locationspointer/controller/LocationController.java
+++ src/main/java/com/jackson/testing_monolith_single_maven/locationspointer/controller/LocationController.java
@@ -1,4 +1,27 @@
 package com.jackson.testing_monolith_single_maven.locationspointer.controller;
 
+import com.jackson.testing_monolith_single_maven.locationspointer.service.LocationPointerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
 public class LocationController {
+
+    @Autowired
+    private LocationPointerService locationPointerService;
+
+    @GetMapping("/nearBy")
+    public ResponseEntity<List<Map<String, Object>>> fetchNearByPOIs(
+            @RequestParam String userAddress,
+            @RequestParam String type
+    ) throws InterruptedException {
+        List<Map<String, Object>> result = locationPointerService.fetchNearByPOIs(userAddress, type);
+        return ResponseEntity.ok(result);
+    }
 }
--- src/main/java/com/jackson/testing_monolith_single_maven/locationspointer/service/LocationPointerService.java
+++ src/main/java/com/jackson/testing_monolith_single_maven/locationspointer/service/LocationPointerService.java
@@ -1,4 +1,15 @@
 package com.jackson.testing_monolith_single_maven.locationspointer.service;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+@Service
 public class LocationPointerService {
+
+
+    public List<Map<String, Object>> fetchNearByPOIs(String userAddress, String type) {
+        return null;
+    }
 }
